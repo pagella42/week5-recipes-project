da7fef35121dbbc26f7fc87ad3abfc518857e520
pagella42 <pagella42@gmail.com>
2019-09-04 11:12:29 -0500

    2

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.get('/sanity', function(req, res){
 app.get('/recipes/:ingredient', function(req, res){
     let ingredient = req.params.ingredient
     request(`https://recipes-goodness.herokuapp.com/recipes/${ingredient}`, function (error, response, body){
-        let result = JSON.parse(body).results.map(r => {return {title: r.title, ingredients: r.ingredients, href: r.href, thumbnail: r.thumbnail}})
+        let result = JSON.parse(body).results.map(r => {
+            return {title: r.title, ingredients: r.ingredients, href: r.href, thumbnail: r.thumbnail}})
         
         res.send(result)
         
